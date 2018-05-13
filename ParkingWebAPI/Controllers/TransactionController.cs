6a505fa307e32a4687491985bc2294a6ba2f1027
Oleg <oleg.cs11@gmail.com>
2018-05-13 23:56:29 +0300

    Added controllers (fixes #1 #2 #3)

---
+++ ParkingWebAPI/Controllers/TransactionController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkingLibrary;
+
+namespace ParkingWebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Transaction")]
+    public class TransactionController : Controller
+    {
+        Parking parking;
+        public TransactionController()
+        {
+            parking = new Parking();
+        }
+
+        // GET: api/Transaction
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return parking.ShowAllTransactions();
+        }
+
+        // GET: api/Transaction/5
+        [HttpGet("GetLastMinuteTransactions")]
+        public IEnumerable<Transaction> GetLastMinuteTransactions()
+        {
+            return parking.GetLastMinuteTransactions();
+        }
+
+        // GET: api/Transaction/5
+        [HttpGet("GetLastMinuteTransactions/{id}")]
+        public IEnumerable<Transaction> GetLastMinuteTransactions(int id)
+        {
+            return parking.GetLastMinuteTransactions(id);
+        }
+
+        // PUT: api/Transaction/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]int sum)
+        {
+            parking.RaiseCarBalance(id, sum);
+        }
+    }
+}
