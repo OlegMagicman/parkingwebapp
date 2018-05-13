a4b51a44b7b390a7463880b8468cf4a5439ea4f6
Oleg <oleg.cs11@gmail.com>
2018-05-13 23:23:57 +0300

    Created Car class (fixes #6)

---
+++ ParkingLibrary/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLibrary
+{
+    public class Car
+    {
+        public int Id { get; }
+        public int Balance { get; private set; }
+        public int CarType { get; }
+
+        public Car(int id, int carType)
+        {
+            this.Id = id;
+            this.Balance = 0;
+            this.CarType = carType;
+        }
+
+        public void ChangeBalance(int count, bool sign)
+        {
+            if (sign == true)
+                this.Balance += count;
+            else
+                this.Balance -= count;
+        }
+    }
+}
