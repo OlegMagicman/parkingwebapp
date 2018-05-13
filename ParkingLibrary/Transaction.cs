9f3f5edf8357d5527772f44ccc8f4ed3baf4b8a0
Oleg <oleg.cs11@gmail.com>
2018-05-13 23:25:51 +0300

    Created Transaction class (fixes #8)

---
+++ ParkingLibrary/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLibrary
+{
+    public class Transaction
+    {
+        public DateTime DateTime { get; }
+        public int CarId { get; }
+        public int Cash { get; }
+
+        public Transaction(int CarId, int Cash)
+        {
+            this.CarId = CarId;
+            this.Cash = Cash;
+            this.DateTime = DateTime.Now;
+        }
+    }
+}
