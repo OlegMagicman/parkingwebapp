8c7e57d1c9db7c7f574231959c9afb249ab93b52
Oleg <oleg.cs11@gmail.com>
2018-05-14 04:30:30 +0300

    Created DataLoadService (close #14)

---
+++ ParkingWebAPI/Services/DataLoadService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ParkingLibrary;
+
+namespace ParkingWebAPI.Services
+{
+    public class DataLoadService
+    {
+        public Parking parking { get; set; }
+
+        private static readonly Lazy<DataLoadService> lazy = new Lazy<DataLoadService>(() => new DataLoadService());
+
+        public static DataLoadService Instance { get { return lazy.Value; } }
+
+        private DataLoadService()
+        {
+            parking = new Parking();
+            TimerCallback TimerDelegate1 = new TimerCallback(parking.TakeFineFromBalance);
+            Timer fineTimer = new Timer(TimerDelegate1, parking, 3000, 3000);
+
+            TimerCallback TimerDelegate2 = new TimerCallback(parking.LogLastMinuteMoney);
+            Timer logTimer = new Timer(TimerDelegate2, parking, 60000, 60000);
+        }
+    }
+}
